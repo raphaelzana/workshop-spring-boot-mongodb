8878c360ebfeeff1263fe6f67c15359052184856
Raphael Zanarelli <raphaelzanarelli@icloud.com>
2018-12-11 23:26:39 -0200

    Criando entity Post com User aninhado

---
+++ src/main/java/com/raphaelzana/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.raphaelzana.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.raphaelzana.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
